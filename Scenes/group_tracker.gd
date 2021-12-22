6e1cc1224696d2211c044b199dca905cc44aeff2
alex91617 <48398157+alex91617@users.noreply.github.com>
2021-12-21 20:52:51 -0500

    Created Group Tracker

---
+++ Scenes/group_tracker.gd
@@ -0,0 +1,25 @@
+extends Area2D
+export var group = ""
+var inRange = []
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.connect("body_entered", self, "areaEntered")
+	self.connect("body_exited", self, "areaExited")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func areaEntered(body):
+	if body.is_in_group(group):
+		inRange.append(body)
+		print(inRange)
+		
+func areaExited(body):
+	if body in inRange:
+		inRange.erase(body)
+		print(inRange)
