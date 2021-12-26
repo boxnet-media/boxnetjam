117d9d282264e88530dbbac918672a73d64c43c3
AlexBowmanCoding <94337821+AlexBowmanCoding@users.noreply.github.com>
2021-12-25 23:18:23 -0500

    Update level_complete.gd

--- level_complete.gd
+++ level_complete.gd
@@ -1,9 +1,12 @@
 extends Area2D
-export var last_level = ""
+export(PackedScene) var last_level 
 
 
 
 
+func _ready():
+	self.connect( "on_body_entered", self, "_on_Area2D_body_entered")
+	
 func _on_Area2D_body_entered(body):
 	if global.is_on_last_level():
 		get_tree().change_scene(last_level)
