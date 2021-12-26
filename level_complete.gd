2c1432b005266479b756b95a62ea977bc3d5521e
AlexBowmanCoding <94337821+AlexBowmanCoding@users.noreply.github.com>
2021-12-26 14:19:47 -0500

    Update level_complete.gd

--- level_complete.gd
+++ level_complete.gd
@@ -1,11 +1,13 @@
 extends Area2D
-export var last_level = "res://Scenes/victory!.tscn"
+
+export var last_level = "res://Scenes/victory.tscn"
 
 
 
 
 func _ready():
 	self.connect( "on_body_entered", self, "_on_Area2D_body_entered")
+	preload("res://Scenes/victory.tscn")
 	
 func _on_Area2D_body_entered(body):
 	if global.is_on_last_level():
